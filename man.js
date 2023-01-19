c14a8a3ec47ac632278f341bfd593290ffaf4bb4
not-damien <93546644+not-damien@users.noreply.github.com>
2023-01-19 12:15:59 -0800

    nitpicking

--- man.js
+++ man.js
@@ -3,13 +3,13 @@ class man{
         this.game = game;
         this.y = y;
         this.x = x;
-        this.animator = new Animator(ASSET_MANAGER.getAsset("./knight.png"), 0, 0, 62, 64, 8, 0.1,0,false,true);
+        this.animator = new Animator(ASSET_MANAGER.getAsset("./knight.png"), 0, 0, 64, 64, 8, 0.3,0,false,true);
         //spritesheet, xStart, yStart, width, height, frameCount, frameDuration, framePadding, reverse, loop
     };
     update(){
 
     };
     draw(ctx){
-        this.animator.drawFrame(this.game.clockTick,ctx,this.x, this.y,1);
+        this.animator.drawFrame(this.game.clockTick,ctx,this.x, this.y,5);
     };
 }
